8b4245a0335b9e60b1b621ae09be150a1a947a59
Ju Dayoung <ekdud7703@naver.com>
2022-01-12 17:37:45 +0900

    war

--- Refri_Refri/src/main/java/com/example/Refri/RefriApplication.java
+++ Refri_Refri/src/main/java/com/example/Refri/RefriApplication.java
@@ -2,10 +2,18 @@ package com.example.Refri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RefriApplication {
+public class RefriApplication extends SpringBootServletInitializer {
 
+	// war 배포를 위한 소스
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RefriApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RefriApplication.class, args);
 	}
